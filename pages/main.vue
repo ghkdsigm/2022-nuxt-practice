2edb0b33ff138012553ad9be2a51eb2567f12486
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-12-04 20:01:18 +0900

    first

---
+++ pages/main.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>홈</div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+}
+</script>
+
+<style></style>
---
+++ pages/product/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>프로덕트페이지</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
