782d54d0f709dd02c2768dd528039e49bf2682af
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-12-05 21:50:00 +0900

    asyncData

--- pages/main.vue
+++ pages/main.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>홈</div>
+  <div>
+    홈
+    <p>{{ product }}</p>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'MainPage',
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    // console.log(response)
+    const product = response.data
+    return { product }
+  },
+  // async created() {
+  //   const response = await axios.get('http://localhost:3000/products')
+  //   console.log(response)
+  //   return { response }
+  // },
 }
 </script>
 
